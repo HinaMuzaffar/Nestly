2b67ac30c17afd8cf9b9802a43bc3570c26de993
HinaMuzaffar <hinaa7095@gmail.com>
2026-04-30 19:48:56 +0500

    owner

--- middleware.js
+++ middleware.js
@@ -25,6 +25,20 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 module.exports.isOwner = async (req, res, next) => {
   const id = req.params.id.trim();
   let listing = await Listing.findById(id);
+  f (!listing) {
+    req.flash("error", "Listing not found.");
+    return res.redirect("/listings");
+  }
+
+  if (!res.locals.currUser) {
+    req.flash("error", "You must be logged in.");
+    return res.redirect("/login");
+  }
+
+  if (!listing.owner) {
+    req.flash("error", "This listing has no owner (data issue).");
+    return res.redirect("/listings");
+  }
   if (!listing.owner._id.equals(res.locals.currUser._id)) {
     req.flash("error", "You are not the owner of the listing.");
     return res.redirect(`/listings/${id}`);
